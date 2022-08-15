5abbbd4aa0835c6ff353ba13dee99104cdaaf561
Diana <dicomeinit@gmail.com>
2022-08-15 12:02:07 +0300

    U.S. States Game Part 2: Challenge with .csv

---
+++ main.py
@@ -0,0 +1,20 @@
+import turtle
+
+screen = turtle.Screen()
+screen.title("U.S. States Game")
+image = "blank_states_img.gif"
+screen.addshape(image)
+turtle.shape(image)
+
+
+answer_state = screen.textinput(title="Guess the State", prompt="What's another state's name?")
+
+# def get_mouse_click_color(x, y):
+#     print(x, y)
+#
+#
+# turtle.onscreenclick(get_mouse_click_color)
+# turtle.mainloop()
+
+screen.exitonclick()
+
